1914f6103509d94474b118664e0d5783ceb3000d
bgar324 <bentgarcia05@gmail.com>
2025-03-17 09:24:41 -0700

    idk

--- pa2/pa2.2.cpp
+++ pa2/pa2.2.cpp
@@ -18,6 +18,7 @@
   i guess what im thinking is that we set something to be the pair, then we recursively go through the array and see if that value at A[0] + A[i] = sum, then if that doesnt happen then we can do okay, part of the pair is then A[1]. the base case is then if we are ever at the end of the array and we havent broken out yet, then we output: "no pair exists"
   */
 
+  //declaring static for left and right as problem does not state if it wants it as parameters or not.
   string findKSum(vector<int>& A, int k) {
     static int left = 0;
     static int right = A.size() - 1;
@@ -26,10 +27,10 @@
 
     if (left >= right) {
       cout << "number of recrusions: " << numRecursions << endl;
-      left = 0, right = A.size() - 1; 
+      left = 0;
+      right = A.size() - 1; 
       numRecursions = 0;
       return "no pair exists";
-      numRecursions = 0;
     }
 
     int sum = A[left] + A[right];
@@ -37,7 +38,8 @@
     if (sum == k) {
         string result = "pair: " + to_string(A[left]) + " and " + to_string(A[right]);
         cout << "number of recrusions: " << numRecursions << endl;
-        left = 0, right = A.size() - 1; 
+        left = 0;
+        right = A.size() - 1; 
         numRecursions = 0;
         return result;
     }
